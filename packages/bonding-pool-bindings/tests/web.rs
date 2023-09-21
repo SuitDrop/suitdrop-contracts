0632043c1514cdb76bf16b4ab748218f7297f542
mccallofthewild <mccallalexander96@gmail.com>
2023-09-21 13:01:40 -0600

    feature: wasm bindgen setup

---
+++ packages/bonding-pool-bindings/tests/web.rs
@@ -0,0 +1,11 @@
+// runs tests in headless browser
+
+#![cfg(target_arch = "wasm32")]
+use wasm_bindgen_test::*;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+#[wasm_bindgen_test]
+fn pass() {
+    assert_eq!(1 + 1, 2);
+}
